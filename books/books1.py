d922deaeffb2b68e03fd16c7c376277b9b41c126
Yasmeen Awad <yasmeenawad@eduroam-171-205.dyn.carleton.edu>
2018-09-17 18:45:25 -0500

    updated stuff

--- books/books1.py
+++ books/books1.py
@@ -1,10 +1,5 @@
 import csv
 import sys
-
-csvName = sys.argv[1]
-
-with open(csvName) as csvfile:
-	reader = csv.reader(csvfile)
 	
 class Book:
 	def __init__(self, title, year, firstName, lastName, lifeTime):
@@ -14,18 +9,65 @@ class Book:
 		self.lastName = lastName
 		self.lifeTime = lifeTime
 
-	def getTitle():
+	def getTitle(self):
 		return self.title
 
-	def getYear():
+	def getYear(self):
 		return self.year
 
-	def getFirstName():
+	def getFirstName(self):
 		return self.firstName
 
-	def getLastName():
+	def getLastName(self):
 		return self.lastName
 
-	def getLifeTime():
+	def getLifeTime(self):
 		return self.lifeTime
 
+# Takes in the name of the csv you want to read, returns a list of Book objects created from information in the csv
+def parseCSV(csvName):
+	#csvName = sys.argv[1]
+	# add try catch here
+	try:
+        with open(csvName) as csvFile:
+            reader = csv.reader(csvFile)
+    except IOError: 
+        sys.stderr.write("Error: file not found.") 
+        exit()
+        
+    library = []
+    for row in reader:
+    authorLine = row[2]
+        splitAuthor = authorLine.split(" ")
+        firstName = splitAuthor[0]
+			
+        # Cases for one author with only one last name
+        if len(splitAuthor) == 3:
+            lastName = splitAuthor[1]
+            lifeTime = splitAuthor[2]
+        else:
+            # Case for one author with two last names
+            if len(splitAuthor) == 4:
+                lastName = splitAuthor[1] + splitAuthor[2]
+				lifeTime = splitAuthor[3]
+            # Case for multiple authors
+            else:
+				lastName = "idk"
+				lifeTime = "idk"
+			
+           
+					
+			
+        book = Book(row[0], row[1], firstName, lastName, lifeTime)
+        library.append(book)
+	return library
+
+
+def test():
+    csvName = sys.argv[1]
+	library = parseCSV(csvName)
+	for book in library:
+		print(book.getFirstName())
+		print(book.getFirstName() + book.getLastName() +  book.getTitle())
+
+test()
