276ca2b4d8baf759b8e0872401ef9190c8f561e9
Chris <padillac3@carleton.edu>
2018-09-19 12:11:46 -0500

    Added oldbooks.csv and fixed some conditionals

--- books/books1.py
+++ books/books1.py
@@ -6,7 +6,7 @@ import csv
 import sys
 
 # This class is for Book objects. Parameters title and year should be strings indicating the title of the book
-# and the year it was published. Authors should be a list of lists containing information about the people who 
+# and the year it was published. Authors should be a list of lists containing information about the people who
 # wrote the book, formatted like so: [[first_name1, last_name1, life_span1], [first_name2, last_name2...]...]
 class Book:
     def __init__(self, title, year, authors):
@@ -25,7 +25,7 @@ class Book:
 
 # Takes in the name of the csv you want to read, returns a list of Book objects created from information in the csv
 def parseCSV(csvName):
-    
+
     # Open csv file
     try:
         csvFile = open(csvName)
@@ -33,7 +33,7 @@ def parseCSV(csvName):
     except IOError:
         sys.stderr.write("Error: file not found.")
         exit()
-    
+
     # Iterate through rows in csv
     library = []
     for row in reader:
@@ -50,7 +50,7 @@ def parseCSV(csvName):
             if len(splitAuthor) == 3:
                 lastName = splitAuthor[1]
                 lifeTime = splitAuthor[2]
-                
+
             # Case for author with two last names
             elif len(splitAuthor) == 4:
                 lastName = splitAuthor[1] + " " + splitAuthor[2]
@@ -58,7 +58,7 @@ def parseCSV(csvName):
             authorObjects.append([firstName, lastName, lifeTime])
 
         library.append(Book(row[0], row[1], authorObjects))
-        
+
     return library
 
 # This function takes in a list of Book objects and returns a list of sorted strings corresponding to the desired output
@@ -70,10 +70,14 @@ def produceOutput(library):
         titles = []
         for book in library:
             titles.append(book.getTitle())
-       
+
         if len(sys.argv) == 4:
             if sys.argv[3] == "reverse":
                 titles.sort(reverse = True)
+            elif sys.argv[3] == "forward":
+                titles.sort()
+            else:
+                errorMessage()
         else:
             titles.sort()
 
@@ -90,6 +94,10 @@ def produceOutput(library):
         if len(sys.argv) == 4:
             if sys.argv[3] == "reverse":
                 authorNames.sort(key=lambda name : name.split()[-1], reverse = True)
+            elif sys.argv[3] == "forward":
+                authorNames.sort(key=lambda name : name.split()[-1])
+            else:
+                errorMessage()
         else:
             authorNames.sort(key=lambda name : name.split()[-1])
 
@@ -98,17 +106,16 @@ def produceOutput(library):
 # Throws an error for bad command line input
 def errorMessage():
     sys.stderr.write("Usage: python3 books1.py input-file action [sort-direction]")
+    exit()
 
 # Main function accounts for command line input and prints output based on command line arguments
 def main():
     # Handling bad input
     if len(sys.argv) > 4 or len(sys.argv) < 3:
         errorMessage()
-        return
     if sys.argv[2] != "books" and sys.argv[2] != "authors":
         errorMessage()
-        return
-    
+
     # Get data from csv and print
     library = parseCSV(sys.argv[1])
     output = produceOutput(library)
