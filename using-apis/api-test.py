82355ab3f3df90128f12b60490dee3163a670517
Yasmeen Awad <yasmeenawad@eduroam-171-205.dyn.carleton.edu>
2018-10-01 22:00:38 -0500

    added most of the code

--- using-apis/api-test.py
+++ using-apis/api-test.py
@@ -1,4 +1,78 @@
 #API Test Program
-# Chris Padilla, 10/1/18
+# Chris Padilla and Yasmeen Awad 10/1/18
+
+import sys
+import json
+import urllib.request
 
 
+def get_countries():
+
+    url = 'https://restcountries.eu/rest/v2/'
+    data_from_server = urllib.request.urlopen(url).read()
+    string_from_server = data_from_server.decode('utf-8')
+    country_list = json.loads(string_from_server)
+
+    result_list = []
+    for country_dictionary in country_list:
+        name = country_dictionary['name']
+
+        if type(name) != type(''):
+            raise Exception('Name is wrong type: "{0}"'.format(name))
+
+        result_list.append({'name': name})
+
+    return result_list
+
+
+def get_country_details(country):
+
+    base_url = 'https://restcountries.eu/rest/v2/name/{0}/'
+    url = base_url.format(country)
+    data_from_server = urllib.request.urlopen(url).read()
+    string_from_server = data_from_server.decode('utf-8')
+    country_list = json.loads(string_from_server)
+
+    result_list = []
+    for country_dictionary in country_list:
+        name = country_dictionary['name']
+        capital = country_dictionary['capital']
+        region = country_dictionary['region']
+        population = country_dictionary['population']
+        area = country_dictionary['area']
+
+        if type(name) != type(''):
+            raise Exception('Name is wrong type: "{0}"'.format(name))
+        if type(capital) != type(''):
+            raise Exception('Capital is wrong type: "{0}"'.format(capital))
+        if type(region) != type(''):
+            raise Exception('Region is wrong type: "{0}"'.format(region))
+        if type(population) != type(0):
+            raise Exception('Population is wrong type: "{0}"'.format(population))
+        if type(area) != type(0):
+            raise Exception('Area has wrong type: "{0}"'.format(area))
+
+        result_list.append({'name': name, 'capital': capital, 'region': region, 'population': population, 'area': area})
+
+    return result_list
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 api-test.py country_name")
+        print("Use 'all' in place of country_name for all countries")
+        exit()
+
+    if sys.argv[1] == "all":
+        country_list = get_countries()
+        for country in country_list:
+            print(country)
+    else:
+        country_details = get_country_details(sys.argv[1])
+        print(country_details)
+
+    #elif sys.argv[1] ==
+
+
+
+if __name__ == '__main__':
+    main()
