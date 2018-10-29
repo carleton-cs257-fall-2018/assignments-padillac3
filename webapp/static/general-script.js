3d4b74fc4576d4c8430b3c04fbb62fc8e83e9abf
Yasmeen Awad <yasmeenawad@eduroam-171-205.dyn.carleton.edu>
2018-10-29 09:42:59 -0500

    added some js code for /map?

--- webapp/static/general-script.js
+++ webapp/static/general-script.js
@@ -1,3 +1,6 @@
+
+
+
 function getBaseURL() {
     var baseURL = window.location.protocol + '//' + window.location.hostname + ':' + "5101"; //TODO CHANGE PORT TO A VARIABLE THAT COMES IN THROUGH COMMAND LINE ARGS
     return baseURL;
@@ -6,7 +9,13 @@ function getBaseURL() {
 
 //Initialize demographics buttons
 
+function map() {
+  var url = getBaseURL() + '/map?' /* + get keyword args */
+  fetch (url, {method: 'get'}).then((response) => response.json()).then(alert(response));
+  
 
+
+}
 
 function random() {
   var url = getBaseURL() + '/random/';
