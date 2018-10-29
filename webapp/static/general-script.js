f8ee13b4ac95ab1a29fe9d233c90579a5dcb49d4
padillac <shinimaninima@gmail.com>
2018-10-29 10:14:58 -0500

    made sure all html button ids and javascript argument calls matched up to be compatible with the API

--- webapp/static/general-script.js
+++ webapp/static/general-script.js
@@ -9,18 +9,29 @@ function getBaseURL() {
 
 //Initialize demographics buttons
 
+//Initialize map
 function map() {
-  var url = getBaseURL() + '/map?' /* + get keyword args */
+  var url = getBaseURL() + '/map/?' + 'demographic=region&interest=' + ''/*ID OF SELECTED INTEREST CATEGORY*/;
+}
+document.getElementById('map').onclick = map;
+
+//Initialize other demographics
+function display_data() {
+  var url = getBaseURL() + '/map/?' + 'interest=' + /*ID OF SELECTED INTEREST CATEGORY*/ + '&demographic=' + ''/*ID OF CLICKED BUTTON*/;
   fetch (url, {method: 'get'}).then((response) => response.json()).then(alert(response));
 
-
-
 }
+document.getElementById('age').onclick = display_data;
+document.getElementById('gender').onclick = display_data;
+document.getElementById('population_density').onclick = display_data;
 
+
+
+//MAYBE GET RID OF THIS SCRIPT IF RANDOM IS MEANT TO LINK TO DIFFERENT HTML PAGE?
+//Initialize random
 function random() {
   var url = getBaseURL() + '/random/';
-  alert(url)
+  alert(url);
   fetch(url, {method: 'get'}).then((response) => response.json()).then(alert(response));
 }
-
 document.getElementById('random').onclick = random;
