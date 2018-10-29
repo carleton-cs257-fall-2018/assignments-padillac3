8b37bc773076f4792c402405b3e379aed1f00c4c
padillac <shinimaninima@gmail.com>
2018-10-29 09:58:46 -0500

    added api_port variable to function in javascript

--- webapp/static/general-script.js
+++ webapp/static/general-script.js
@@ -2,7 +2,7 @@
 
 
 function getBaseURL() {
-    var baseURL = window.location.protocol + '//' + window.location.hostname + ':' + "5101"; //TODO CHANGE PORT TO A VARIABLE THAT COMES IN THROUGH COMMAND LINE ARGS
+    var baseURL = window.location.protocol + '//' + window.location.hostname + ':' + api_port;
     return baseURL;
 }
 
@@ -12,7 +12,7 @@ function getBaseURL() {
 function map() {
   var url = getBaseURL() + '/map?' /* + get keyword args */
   fetch (url, {method: 'get'}).then((response) => response.json()).then(alert(response));
-  
+
 
 
 }
