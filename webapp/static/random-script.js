40256839a7cc5d281b74da319524e2772ebef2f2
padillac <shinimaninima@gmail.com>
2018-10-29 19:48:57 -0500

    added random-script.js

--- webapp/static/random-script.js
+++ webapp/static/random-script.js
@@ -2,6 +2,6 @@ alert("random page!")
 
 //Initialize home page button
 function go_to_home_page() {
-  document.location.href = getBaseURL();
+  document.location.href = getBaseAPIURL();
 }
 document.getElementById('go_to_home_page').onclick = go_to_home_page;
