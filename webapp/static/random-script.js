4a3444f025b6a0e49d4bfa3a4951a8ce4e5e8914
padillac <shinimaninima@gmail.com>
2018-10-29 20:56:33 -0500

    lalla

--- webapp/static/random-script.js
+++ webapp/static/random-script.js
@@ -1,5 +1,15 @@
 alert("random page!")
 
+function getBaseAPIURL() {
+    var baseURL = window.location.protocol + '//' + window.location.hostname + ':' + api_port;
+    return baseURL;
+}
+
+function getBaseWebURL() {
+  var baseURL = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port;
+  return baseURL;
+}
+
 //Initialize home page button
 function go_to_home_page() {
   document.location.href = getBaseWebURL();
