486781b979b59695325905fb89e78cbf7481d05c
Yasmeen Awad <yasmeenawad@eduroam-171-205.dyn.carleton.edu>
2018-10-30 16:21:13 -0500

    j

--- webapp/static/random-script.js
+++ webapp/static/random-script.js
@@ -19,6 +19,7 @@ function display_story() {
   var url = getBaseAPIURL() + "/random";
   fetch(url, {method: 'get'}).then((response) => response.json()).then(function(story) {
     document.getElementById('story').innerHTML = story;
+    alert(story);
   });
 }
 
